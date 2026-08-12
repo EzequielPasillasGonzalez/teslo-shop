1bc451c344dc27e70e8f09f7281bf7b3a8ca6931
Ezequiel <ingepasillasg@gmail.com>
2026-08-11 22:10:31 -0600

    feat: componentes para mostrar los productos

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,13 @@
 import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={products} />
       <CustomPagination totalPages={7} />
     </>
   );
