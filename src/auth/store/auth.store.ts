515dc8c03009f7368339ee3f888afac3a333ac90
Ezequiel <ingepasillasg@gmail.com>
2026-08-17 10:31:58 -0600

    instalacion de zuztand

--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -1,12 +1,13 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 type Store = {
-  count: number
-  inc: () => void
-}
+  count: number;
+  inc: () => void;
+  dec: () => void;
+};
 
-export  const useStore = create<Store>()((set) => ({
+export const useCounterStore = create<Store>()((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
-}))
-
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,5 +1,16 @@
+import { useCounterStore } from "@/auth/store/auth.store.ts";
+
 const ProductPage = () => {
-  return <div>ProductPage</div>;
+  const { count, inc, dec } = useCounterStore();
+
+  return (
+    <>
+      <h1 className="text-3xl font-monserrat">Count: {count}</h1>
+
+      <button onClick={inc}>+1</button>
+      <button onClick={dec}>-1</button>
+    </>
+  );
 };
 
 export default ProductPage;
