4a0c3365cdf7ede08720de36819b43708722a02b
Ezequiel <ingepasillasg@gmail.com>
2026-08-20 09:19:17 -0600

    feat: optimizacion de product form

--- src/admin/pages/product/ui/AdminProdcutForm.tsx
+++ src/admin/pages/product/ui/AdminProdcutForm.tsx
@@ -5,7 +5,7 @@ import { X, Plus, Upload, Tag, SaveAll } from "lucide-react";
 
 import { AdminTitle } from "@/admin/components/AdminTitle";
 import { Button } from "@/components/ui/button";
-import type { Product } from "@/interfaces/product.interface.ts";
+import type { Product, Size } from "@/interfaces/product.interface.ts";
 import { cn } from "@/lib/utils.ts";
 interface Props {
   title: string;
@@ -13,7 +13,7 @@ interface Props {
   product: Product;
 }
 
-const availableSizes = ["XS", "S", "M", "L", "XL", "XXL"];
+const availableSizes: Size[] = ["XS", "S", "M", "L", "XL", "XXL"];
 
 export const AdminProdcutForm = ({ product, subtitle, title }: Props) => {
   const [dragActive, setDragActive] = useState(false);
@@ -22,9 +22,14 @@ export const AdminProdcutForm = ({ product, subtitle, title }: Props) => {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
+    setValue,
+    watch,
   } = useForm({
     defaultValues: product,
   });
+
+  const selectedSizes = watch("sizes");
 
   const addTag = () => {
     // if (newTag.trim() && !product.tags.includes(newTag.trim())) {
@@ -43,13 +48,10 @@ export const AdminProdcutForm = ({ product, subtitle, title }: Props) => {
     // }));
   };
 
-  const addSize = (size: string) => {
-    if (!product.sizes.includes(size)) {
-      // setProduct((prev) => ({
-      //   ...prev,
-      //   sizes: [...prev.sizes, size],
-      // }));
-    }
+  const addSize = (size: Size) => {
+    const sizeSet = new Set(getValues("sizes"));
+    sizeSet.add(size);
+    setValue("sizes", Array.from(sizeSet));
   };
 
   const removeSize = (sizeToRemove: string) => {
@@ -268,10 +270,15 @@ export const AdminProdcutForm = ({ product, subtitle, title }: Props) => {
 
               <div className="space-y-4">
                 <div className="flex flex-wrap gap-2">
-                  {product.sizes.map((size) => (
+                  {availableSizes.map((size) => (
                     <span
                       key={size}
-                      className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-800 border border-blue-200"
+                      className={cn(
+                        "inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-800 border border-blue-200",
+                        {
+                          hidden: !selectedSizes.includes(size),
+                        },
+                      )}
                     >
                       {size}
                       <button
@@ -290,11 +297,12 @@ export const AdminProdcutForm = ({ product, subtitle, title }: Props) => {
                   </span>
                   {availableSizes.map((size) => (
                     <button
+                      type="button"
                       key={size}
                       onClick={() => addSize(size)}
-                      disabled={product.sizes.includes(size)}
+                      disabled={getValues("sizes").includes(size)}
                       className={`px-3 py-1 rounded-full text-sm font-medium transition-all duration-200 ${
-                        product.sizes.includes(size)
+                        selectedSizes.includes(size)
                           ? "bg-slate-100 text-slate-400 cursor-not-allowed"
                           : "bg-slate-200 text-slate-700 hover:bg-slate-300 cursor-pointer"
                       }`}
